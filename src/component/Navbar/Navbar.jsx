bfec5ef81e1f3a59d39e9fcb1407dc9db21ac458
rifat76511 <rifatalvi765@gmail.com>
2026-04-01 00:12:10 +0600

    first commit

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { LuShoppingCart } from 'react-icons/lu';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Products</a></li>
+                        <li><a>Features</a></li>
+                        <li><a>Pricing</a></li>
+                        <li><a>Testimonials</a></li>
+                        <li><a>FAQ</a></li> 
+                    </ul>
+                </div>
+                <a className="font-bold liner-gr text-xl  bg-linear-to-r text-transparent bg-clip-text from-[#4F39F6] to-[#9514FA]">DigiTools</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Products</a></li>
+                    <li><a>Features</a></li>
+                    <li><a>Pricing</a></li>
+                    <li><a>Testimonials</a></li>
+                    <li><a>FAQ</a></li>
+
+                </ul>
+            </div>
+            <div className="navbar-end gap-5 relative">
+                <LuShoppingCart  />
+                   <span className='absolute top-2 right-44 bg-red-500 rounded-full font-light px-1 text-[8px] text-white'>0</span>
+              
+                <h2>Login </h2>
+                <a className="btn bg-linear-to-r  from-[#4F39F6] to-[#9514FA] text-white rounded-full">Get Started</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
